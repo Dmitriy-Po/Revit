81c509a41e6b214c3bff150b66686c680a56fc92
Dmitriy Po <gektor775@yandex.ru>
2020-09-06 15:28:40 +0500

    Добавленна кнопка разделения полутона на квартирах. - RibbonBtnSetNewColorRoom/

--- ClassLibrary1/RibbonButtonsAddons.cs
+++ ClassLibrary1/RibbonButtonsAddons.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Dynamic;
-using System.IO;
-using System.Threading.Tasks;
-using System.Windows.Media.Imaging;
-using Autodesk.Revit.Attributes;
+﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
 using ClassLibrary1.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Media.Imaging;
 
 
 namespace ClassLibrary1
@@ -173,6 +173,85 @@ namespace ClassLibrary1
         }
     }
 
+    [Transaction(TransactionMode.Manual)]
+    class RibbonBtnSetNewColorRoom : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document DOC = commandData.Application.ActiveUIDocument.Document;
+            var allRooms = new FilteredElementCollector(DOC)
+                   .WherePasses(new RoomFilter())
+                   .Where(w => w.LookupParameter("ROM_Зона").AsString().Contains("Квартира"))
+                   .ToList()
+                      .GroupBy
+                      (
+                          g => new
+                          {
+                              Уровнеь = g.GetParameters("Уровень").FirstOrDefault().Element.LevelId,
+                              BS_Блок = g.GetParameters("BS_Блок").FirstOrDefault().AsString(),
+                              ROM_Подзона = g.GetParameters("ROM_Подзона").FirstOrDefault().AsString(),
+                              ROM_Зона = g.GetParameters("ROM_Зона").FirstOrDefault().AsString()
+                          }
+                      )
+                   .OrderBy(o => o.Key.ROM_Зона)
+                   .ThenBy(t => t.Key.BS_Блок)
+                   .ThenBy(t => t.Key.ROM_Подзона)
+                   .ThenBy(t => t.Key.ROM_Зона);
+
+            string previorsRoom = "";
+
+            using (Transaction transaction = new Transaction(DOC, "Set new Values"))
+            {
+                transaction.Start();
+                foreach (var groupOfRooms in allRooms)
+                {
+                    if (previorsRoom != "")
+                    {
+                        // Если текущий элемент, по значению ROM_Зона совпадает с предыдущим, то это значит, что комнаты имеют сквозную нумерацию.
+                        // Уточнение: условие выполнится если, значение текущего элемента по параметру ROM_Зона уменьшенного на единицу, совпадает с предыдущим элементом.
+                        // Пример: текущий_элемент.Квартира (07 -1) == предыдущий_элемент.Квартира (06).					
+                        if (ROMStringToInt(groupOfRooms.Key.ROM_Зона) - 1 == ROMStringToInt(previorsRoom))
+                        {
+                            SetNewValues(groupOfRooms);
+                        }
+                    }
+                    previorsRoom = groupOfRooms.Key.ROM_Зона;
+                }
+                transaction.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+
+
+        private void SetNewValues(IGrouping<object, Element> groupOfRooms)
+        {
+            foreach (var room in groupOfRooms)
+            {
+                Parameter ROM_index = room.GetParameters("ROM_Подзона_Index").FirstOrDefault();
+                string ROM_under_index = room.GetParameters("ROM_Подзона_ID").FirstOrDefault().AsString();
+
+                // Присовить формат будущего значения, например: "2к.Полутон".
+                // TODO: добавить проверку данного, если такое значение уже существует.
+                ROM_index.Set(ROM_under_index + ".Полутон");
+            }
+        }
+
+
+        /// <summary>
+		/// Функция для преобразования строкового представления в целое, по параметру - ROM_Зона.
+		/// </summary>
+		/// <returns></returns>
+		private int ROMStringToInt(string nameRoom)
+        {
+            // Номер квартиры получаем через первый найденный пробел, после которого, следует номер квартиры.
+            // TODO сделать проверку через регулярные выражения.
+            string numberRoom = nameRoom.Substring(nameRoom.IndexOf(" "));
+
+            return int.Parse(numberRoom);
+        }
+    }
+
     /// <summary>
     /// Класс для кнопок.
     /// </summary>
@@ -188,7 +267,7 @@ namespace ClassLibrary1
                 typeof(B).FullName);
         }
 
-        public Result OnShutdown(UIControlledApplication application) => Result.Succeeded;        
+        public Result OnShutdown(UIControlledApplication application) => Result.Succeeded;
 
         public Result OnStartup(UIControlledApplication application)
         {
@@ -209,10 +288,15 @@ namespace ClassLibrary1
             PushButton btnShowHide = (PushButton)panelA.AddItem(
                 CreateButton<RibbonBtnShowHide>("Показать категории", fileDLL));
 
+            PushButton btnNewColor = (PushButton)panelA.AddItem(
+                CreateButton<RibbonBtnSetNewColorRoom>("Выделить подходящие квартиры", fileDLL));
+
             string pathImage = @"C:\Users\Дмитрий\Desktop\Revit\Revit2016APITraining\Labs\2_Revit_UI_API\Images";
             btnShowRooms.LargeImage = new BitmapImage(new Uri(Path.Combine(pathImage, "ImgHelloWorld.png"), UriKind.Absolute));
             btnShowAllWalls.LargeImage = new BitmapImage(new Uri(Path.Combine(pathImage, "Elements.ico"), UriKind.Absolute));
             btnShowHide.LargeImage = new BitmapImage(new Uri(Path.Combine(pathImage, "Parameters.ico"), UriKind.Absolute));
+            btnNewColor.LargeImage = new BitmapImage(new Uri(Path.Combine(pathImage, "eye.png")));
+
 
             return Result.Succeeded;
         }
